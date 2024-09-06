fc158c19213ff7c7ea10573da17273185d2e54a3
Summit1122 <155012691+Summit1122@users.noreply.github.com>
2024-09-06 15:16:49 -0500

    Submission for week 1: Luis Amador
    Not sure If I did this correctly but here you go.
    
    - Luis Amador

---
+++ Program_1.py
@@ -0,0 +1,5 @@
+#Luis Amador
+#9/5/24
+# Title: Hello World
+
+print("Hello World")
---
+++ Program_2.py
@@ -0,0 +1,7 @@
+#Luis Amador
+#9/5/24
+#TITLE: Bible Verse
+print('"Do you see a man skillful in his work?')
+print("He will stand before kings;")
+print('he will not stand before obscure men."')
+print('   (\033[1mProverbs 22:29\033[0m)   ')
