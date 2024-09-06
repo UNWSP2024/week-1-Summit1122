b7fca15de5b21950e4bebb7641532e4b94346396
Summit1122 <155012691+Summit1122@users.noreply.github.com>
2024-09-06 15:25:43 -0500

    Last minute change

--- Program_2.py
+++ Program_2.py
@@ -5,3 +5,11 @@ print('"Do you see a man skillful in his work?')
 print("He will stand before kings;")
 print('he will not stand before obscure men."')
 print('   (\033[1mProverbs 22:29\033[0m)   ')
+
+# when clicking run
+# Output should be
+#  "Do you see a man skillful in hit work?
+#  He will stand before kings;
+#  he will not stand before obscure men."
+#        (Proverbs 22:29)
+# The verse number will be in bold 
--- program_2.py
+++ program_2.py
@@ -1,6 +1,15 @@
-def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    print('...')
-    
+#Luis Amador
+#9/5/24
+#TITLE: Bible Verse
+print('"Do you see a man skillful in his work?')
+print("He will stand before kings;")
+print('he will not stand before obscure men."')
+print('   (\033[1mProverbs 22:29\033[0m)   ')
 
-proverbs_22_29()
+# when clicking run
+# Output should be
+#  "Do you see a man skillful in hit work?
+#  He will stand before kings;
+#  he will not stand before obscure men."
+#        (Proverbs 22:29)
+# The verse number will be in bold 
